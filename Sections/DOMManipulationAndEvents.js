5a64b1813c9791275625a60fa8da7c761ba0d386
RiskTolerance <isaac.druin@gmail.com>
2020-12-16 15:04:00 -0600

    Reviewing DOM, forgot to commit at eod yesterday

---
+++ Sections/DOMManipulationAndEvents.js
@@ -0,0 +1,84 @@
+// let val;
+
+// val = document;
+// val = document.all;
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList[0];
+
+// val = document.images;
+
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
+
+// let scripts = document.scripts;
+
+// let scriptsArr = Array.from(scripts);
+
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
+
+// console.log(val);
+
+// 3.23 Examining the Document Object
+
+
+
+// 3.24 Dom Selectors For Single Elements
+
+
+
+// 3.25 Dom Selectors For Multiple Elements
+
+
+
+// 3.26 Traversing The DOM
+
+
+
+// 3.27 Creating Elements
+
+
+
+// 3.28 Removing & Replacing Elements
+
+
+
+// 3.29 Event Listeners & The Event Object
+
+
+
+// 3.30 Mouse Events
+
+
+
+// 3.31 Keyboard & Input Events
+
+
+
+// 3.32 Event Bubbling & Delegation
+
+
+
+// 3.33 Local & Session Storage
+
+
